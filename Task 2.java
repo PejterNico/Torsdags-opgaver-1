0c7cba2908b697950342f268f9004a8ad6ecbe9a
PejterNico <124774197+PejterNico@users.noreply.github.com>
2023-02-09 16:11:59 +0100

    Tor Opg 1

---
+++ Task 2.java
@@ -0,0 +1,26 @@
+// Task 2
+void setup() {
+// 2.a - declare age and ;print name
+String name = ("Nicolai");
+println(name);
+
+// 2.b - declare age and print age
+int age = 22;
+println(age);
+
+// 2.c - boolean happe or sad
+String dont = ("happy");
+if (dont == "happy") {
+  println(dont);
+}  else if(dont == "sad"); {
+    println(dont);
+  }
+
+// 2.d - print 3 text lines
+println("Hi, my name is " + name);
+println("I am " + age + " years old");
+println("I " + dont + " clap my hands");
+
+
+}
+  
